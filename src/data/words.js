39ee51b53ffbe934d60d9646be4c5874070e35bc
GabrielR4SH <bieelsantana23@hotmail.com>
2023-06-30 14:17:06 -0300

    First Commit, palavras definidas

---
+++ src/data/words.js
@@ -0,0 +1,9 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capo", "Peneu", "Antena"],
+    fruta: ["Banana","Maçã","Pera","Uva","Laranja","Mamão"],
+    corpo: ["Braço","Perna","Unha","Dente","Mãos","Pés"],
+    computador: ["Mouse","Processador","Teclado","Monitor"],
+    alimento: ["Arroz","Feijão","Carne","Leite","Ovo"]
+
+}
+
