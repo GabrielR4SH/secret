13f205d456039143fa78f18627c9cc25be32db42
GabrielR4SH <bieelsantana23@hotmail.com>
2023-07-03 00:04:47 -0300

    Palavras capturadas e inseridas com tema

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { useCallback, useEffect, useState } from 'react';
 import { wordsList } from './data/words';
 
 //GameStates
-const stages = [
+const stages = [ 
   {id:1, name: "start"},
   {id:2, name: "game"},
   {id:3, name: "end"},
@@ -18,10 +18,42 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name); //Setting Stage 'Start'
   const [words] = useState(wordsList); //Datas Recives
 
-  console.log(wordsList)
+  
+
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("")
+  const [letters, setLetters] = useState([]);
+
+
+  const pickedWordAndCategory = () => {
+    const categories = Object.keys(words)
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    console.log(category)
+
+    const word = words[category][Math.floor(Math.random() * Object.keys(categories).length)]
+
+    return{word, category}
+  }
 
   //start Game
   const startGame = () => {
+    
+    // pick word and pick category
+    const {word, category} = pickedWordAndCategory();
+
+    // create an array of letters
+    let wordLetters = word.split("");
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    console.log(word, category);
+    console.log(wordLetters);
+
+    setPickedWord(word)
+    setPickedCategory(category)
+    setLetters(letters)
+
     setGameStage(stages[1].name)
   }
 
